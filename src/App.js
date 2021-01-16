73cd0b05d378611024e3781472eb0128e94356e1
Shivangi Agrawal <shivangiagrawal667@gmail.com>
2021-01-16 21:01:23 +0530

    Initial Commit: Codebase Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <span>Weather App</span>
+      </div>
+      <div className="body-container">
+        <input placeholder="Enter City Name..."></input>
+        <button>Search</button>
+      </div>
     </div>
   );
 }
